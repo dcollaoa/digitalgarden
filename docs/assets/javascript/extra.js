237b9689715dcc238e2bcbaac9512764186aa194
unknown <diego.collao.albornoz@gmail.com>
2024-07-28 17:40:57 -0400

    Analyticis and other fixs

--- docs/assets/javascript/extra.js
+++ docs/assets/javascript/extra.js
@@ -1,3 +1,9 @@
 document$.subscribe(() => {
   hljs.highlightAll()
 })
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-0F47J154E5');
---
+++ docs/pwnnet.md
@@ -0,0 +1,1 @@
+icon:material/tshirt-crew
